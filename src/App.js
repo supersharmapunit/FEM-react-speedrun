9a00d5cc8c2bf9789863319666e50e7865d480ae
supersharmapunit <50194646+supersharmapunit@users.noreply.github.com>
2023-12-08 17:26:42 +0530

    initial js code tools config and react through CDN

---
+++ src/App.js
@@ -0,0 +1,28 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Tiger",
+      breed: "Labradore",
+    }),
+    React.createElement(Pet, { animal: "Bird", name: "Tota", breed: "Parrot" }),
+    React.createElement(Pet, {
+      animal: "Fish",
+      name: "Goldy",
+      breed: "Gold-fish",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App, {}, null));
