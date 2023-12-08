00beabdaecaf7fc2e0de321d85322bb2d57b71d0
supersharmapunit <50194646+supersharmapunit@users.noreply.github.com>
2023-12-09 01:35:28 +0530

    changes in eslintrc for rules and converted code to modular react code

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,17 @@
-import React from "react";
 import { createRoot } from 'react-dom';
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      animal: "Dog",
-      name: "Tiger",
-      breed: "Labradore",
-    }),
-    React.createElement(Pet, { animal: "Bird", name: "Tota", breed: "Parrot" }),
-    React.createElement(Pet, {
-      animal: "Fish",
-      name: "Goldy",
-      breed: "Gold-fish",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name = "Tiger" animal = "Dog" breed = "Labrador"/>
+      <Pet name = "Tota" animal = "Bird" breed = "Parrot"/>
+      <Pet name = "Goldy" animal = "Fish" breed = "Gold-fish"/>
+    </div>
+  )
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App, {}, null));
+root.render(<App/>);
